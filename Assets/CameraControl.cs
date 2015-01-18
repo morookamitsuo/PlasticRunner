f229edb1fed5cad65ff3ac3a427bb829a77aa0cd
Mitsuo Morooka <morookamitsuo1@morookamitsuoi72-2.local>
2015-01-19 00:31:54 +0900

    first_commit
    first_commit

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+		private GameObject player = null;
+		private Vector3 position_offset = Vector3.zero;
+
+		void Start ()
+		{
+				// メンバー変数playerに、playerオブジェクトを取得
+				this.player = GameObject.FindGameObjectWithTag ("Player");
+				// カメラの位置（this.transform.position）と
+				// プレイヤーの位置(this.player.transdorm.position)との差分を保管
+				this.position_offset = this.transform.position - this.player.transform.position;
+		}
+
+		void LateUpdate ()
+		{
+				/*Vector3 new_position = this.transform.position;
+				 * new_position.x= this.player.transform.position.x + this.position_offset.x;
+				 * this.transform.position = new_position;
+				*/
+		}
+
+		void Update ()
+		{
+				// カメラの現在位置をnew_positionに取得
+				Vector3 new_position = this.transform.position;
+				Debug.Log ("camera position= " + new_position);
+
+				// プレイヤーのX座標に差分を足して、変数new_positionのXに代入する。
+				new_position.x = player.transform.position.x + this.position_offset.x;
+				// カメラの位置を、新しい位置(new_position)に更新。
+				this.transform.position = new_position;
+
+
+		}
+}
