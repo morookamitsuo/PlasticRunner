51226b6f9428e357b9aa72d2e0c36edfe3bdaf0d
Mitsuo Morooka <morookamitsuo1@morookamitsuoi72.local>
2015-03-07 20:40:28 +0900

    完成版

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -226,4 +226,15 @@ public class PlayerControl : MonoBehaviour
 				} while(false);
 				// ループの脱出口
 		}
+
+		public bool isPlayEnd () // ゲームが終わったかどうかを判定する
+		{ 
+				bool ret = false;
+				switch (this.step) {
+				case STEP.MISS: // MISS状態なら
+						ret = true; // 「死んだよー」(true)と返す。
+						break;
+				}
+				return(ret);
+		}
 }
---
+++ Assets/TitleScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+	
+	void Start () {
+
+	}
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Application.LoadLevel("GameScene");
+		}
+	}
+
+	void OnGUI(){
+		GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 128, 32),"PlasicRunner");
+	}
+}
